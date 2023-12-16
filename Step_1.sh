bd7ad583387e9cc103cd8e3105e1fcc9cc423aef
Vaidehi Pusadkar <98483312+VaidehiPusadkar@users.noreply.github.com>
2023-12-15 18:16:52 -0600

    Update Step_1.sh

--- Step_1.sh
+++ Step_1.sh
@@ -1,21 +1,36 @@
 ulimit -s unlimited
 
-markov_order=$1 
-confid1=$2
-confid2=$3
-confid3=$4
+#markov_order=$1 
+#confid1=$2
+#confid2=$3
+#confid3=$4
 
 ######## Segmentation and clustering of each genome #######################################
 echo ">>starting segmentation and clustering"
-cc Seg_clus/seg_clus.c -lm
-for file in Base_Genomes/*.fna; do ./a.out "$file" $markov_order $confid1 $confid2 $confid3;done >out
-rm Base_Genomes/*.seg.txt
+
+cc Seg_clus/seg_clus.c -lm -o sgm
+
+for file in db_Genomes/*.fna; do ./sgm "$file" 2 0.90 0.95 0.99;done >out
 
 echo ">>segmentation-clustering completed."
 
 ######## creating the fasta sequence file for each cluster ############
 
-for file in Base_Genomes/*.fna; do 
-perl Seg_clus/seqextract.pl $file $file.clustering2.txt tempfile.txt >LL
+for file in db_Genomes/*.fna; do 
 
-perl Seg_clus/spilit.pl tempfile.txt;done
+perl Seg_clus/extract.pl $file $file.clustering2.txt tempfile.txt >LL
+
+perl Seg_clus/clustersplit.pl tempfile.txt;done
+
+########### Moving the files to the different folder ##################
+
+mkdir clustered_genomes
+mv db_Genomes/*.fa clustered_genomes/
+
+###### remove the unwanted files ###############
+rm db_Genomes/*.seg.txt
+rm db_Genomes/*clustering2.txt
+rm tempfile.txt LL seq.txt seq1.txt out
+
+######### generate a lxt file for generating a list of genome cluster files which will be used for by taxonomic classification step ################# 
+ls -d $PWD/clustered_genomes/* > Genomes.txt
